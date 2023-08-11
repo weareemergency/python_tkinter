aa971926bdee412667e9ebb56b8aa39aa8ea859f
jiseok12 <leejiseok2006@gmail.com>
2023-08-11 22:43:20 +0900

    health_report.py

--- health_report.py
+++ health_report.py
@@ -39,7 +39,16 @@ class health_list:
         self.health_label = Label(self.root, font=('NanumGothic', 25, 'bold'), text='건강보고함      >',fg="white", bg="#1b1b1b")
         self.note_icon = Label(self.root,image=self.root.note, bg="#1b1b1b", borderwidth=0, highlightthickness=0)
         self.ract_image = Label(self.root, image=self.root.ract, bg="#1b1b1b", borderwidth=0, highlightthickness=0)
+        self.health_con = Label(self.root, font=('NanumGothic', 23, 'bold'), text='양유빈님의 현재 건강 상태',fg="black", bg="white")
+        self.con_en = Label(self.root, font=('NanumGothic', 18, 'bold'), text='Physical condition',fg="#7c7c7c", bg="white")
+        self.con_list = Listbox(self.root, font=('NanumGothic', 19) ,width=54, height=18, borderwidth=1,bg="white",highlightthickness=0)
         
+        self.con_list.insert(END, '-올해 3월 경추추간판')
+        self.con_list.insert(END, '-탈출증 초기 및 허리뼈')
+        
+        self.canvas.create_window(540, 1020, window=self.con_list)
+        self.canvas.create_window(600,700, window=self.con_en)
+        self.canvas.create_window(300,700, window=self.health_con)
         self.canvas.create_window(260, 620, window=self.note_icon)
         self.canvas.create_window(193, 620, window=self.health_label)
         self.canvas.create_window(540, 990, window=self.ract_image)
